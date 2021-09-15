e555f62d1e239652271e43d3f5d1ebc742ec3588
benaiken <90015778+benaiken@users.noreply.github.com>
2021-09-15 18:58:17 -0400

    Completed!

--- 03_neighborhoods_with_most_restaurants.sql
+++ 03_neighborhoods_with_most_restaurants.sql
@@ -9,7 +9,19 @@
 */
 
 with business_annotated_with_neighborhood as (
-    select ...
+  select
+  n.name,
+  count(*) as num_restaurants
+  from business_licenses b
+  join neighborhoods_philadelphia n
+  on st_contains(n.the_geom, b.the_geom)
+  Where licensetyp like 'Food Preparing and Serving%'
+  Group by n.name
 )
 
-select ...
+select n.*
+from business_annotated_with_neighborhood b
+join neighborhoods_philadelphia n
+on n.name = b.name
+order by num_restaurants desc
+limit 5
