1b3dffd3771a8de856f2dc7a12053a4a59b05571
Mjumbe Poe <mjumbewu@gmail.com>
2021-09-16 18:30:48 -0400

    Third query

--- 03_neighborhoods_with_most_restaurants.sql
+++ 03_neighborhoods_with_most_restaurants.sql
@@ -9,7 +9,15 @@
 */
 
 with business_annotated_with_neighborhood as (
-    select ...
+    select n.name as neighborhood_name, b.*
+	from phl_business_licenses as b
+	join phl_neighborhoods as n
+		on st_contains(n.the_geom, b.the_geom)
 )
 
-select ...
+select neighborhood_name, count(*) as num_restaurants
+from business_annotated_with_neighborhood
+where licensetype like 'Food Preparing and Serving%'
+group by 1
+order by 2 desc
+limit 5
